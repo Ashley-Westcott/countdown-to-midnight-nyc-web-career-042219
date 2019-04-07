bf6e315b1e27d63e8b33158986765555ae6df192
Ashley Westcott <ashley.e.westcott@gmail.com>
2019-04-07 00:47:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -11,9 +11,10 @@ end
 
 def countdown_with_sleep(number)
   while number > 0
+  sleep 1
   puts "#{number} SECOND(S)!"
   break if number == 0
-  sleep 1 number -= 1
+   number -= 1
   end
   return "HAPPY NEW YEAR!"
 end
